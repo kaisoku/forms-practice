5958502fde5574398ad834d3b2a8e8c000ca1fef
Kaisoku <khuss063@gmail.com>
2023-04-18 16:37:48 +0200

    dev practice td form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'forms-practice';
+  @ViewChild('f') formData: NgForm;
+  defaultLevel = 'Advanced';
+  formSubmitted = false;
+  user = {
+    email: '',
+    level: this.defaultLevel,
+    password: '',
+  };
+  onSubmit() {
+    console.log(this.formData.value);
+    this.formSubmitted = true;
+    this.user.email = this.formData.value.userData.email;
+    this.user.level = this.formData.value.userData.level;
+    this.user.password = this.formData.value.userData.password;
+  }
 }
